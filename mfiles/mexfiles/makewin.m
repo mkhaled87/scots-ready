689bd35a4b99bd8e5a1fbe10ca202d8ba4ca617f
Mahmoud Khaled <eng.mk@msn.com>
2020-11-06 15:41:16 +0100

    mods

--- mfiles/mexfiles/makewin.m
+++ mfiles/mexfiles/makewin.m
@@ -8,17 +8,10 @@ clc;
 
 disp('Building the MATLAB interface for Windows ... ');
 
-% the ported CUDD
-cuddProjPath = fullfile('..','..','cudd-3.0.0');
-cuddIncPath =  fullfile(cuddProjPath);
-cuddLibPath =  fullfile(cuddProjPath, 'lib', 'cudd.lib');
-
-
-% path of includes
+% paths of includes
+cudd_path = fullfile('..','..','cudd-3.0.0');
 ipath_bdd  = ['-I' fullfile('..','..','bdd')];
-ipath_cudd = ['-I' cuddIncPath];
+ipath_cudd = ['-I' cudd_path];
 
-% linking with CUDD. You need to have the library in the path
-linkcudd = ['-l' cuddLibPath];
-
-mex('-v',ipath_bdd, ipath_cudd, linkcudd , 'mexSymbolicSet.cc')
+% build all (including the CUDD) and link inside one file
+mex('-v', ipath_bdd, ipath_cudd, '-lws2_32', 'mexSymbolicSet.cc', fullfile(cudd_path, '*.cc'), fullfile(cudd_path, '*.c'))
